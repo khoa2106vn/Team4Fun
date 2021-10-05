e2f4254b7654005f45b5b960e284de1cdbda13b3
Khoa <khoa2106vn@gmail.com>
2021-10-05 16:46:33 +0700

    added font-awesome

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 	@auth
+		<link rel="stylesheet" href="../fontawesome/css/all.css" >
 		<link rel="stylesheet" href="css/sidebar.css">
 		<div class="sidenav">
 			<div class="items-center">
-				<a href="#">Friends</a>
-				<a href="#">Posts</a>
-				<a href="#">Messengers</a>
-				<a href="#">Contact</a>
+				<a href="{{ route('profile') }}"> <i class="fas fa-id-card-alt"></i> Profile</a>
+				<a href="#"> <i class="fas fa-user-friends"></i> Friends</a>
+				<a href="{{ route('posts') }}"> <i class="fas fa-mail-bulk"></i> Posts</a>
+				<a href="#"> <i class="fas fa-comment-alt"></i> Messengers</a>
+				<a href="#"> <i class="fas fa-phone"></i> Contact</a>
 			</div>
 		</div>
 	@endauth
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+	<div>
+		<link rel="stylesheet" href="../fontawesome/css/all.css" >
+		<link rel="stylesheet" href="css/sidebar.css">
+		<div class="sidenav">
+			<div class="items-center">
+				<a href="{{ route('profile') }}"> <i class="fas fa-id-card-alt"></i> Profile</a>
+				<a href="#"> <i class="fas fa-user-friends"></i> Friends</a>
+				<a href="{{ route('posts') }}"> <i class="fas fa-mail-bulk"></i> Posts</a>
+				<a href="#"> <i class="fas fa-comment-alt"></i> Messengers</a>
+				<a href="#"> <i class="fas fa-phone"></i> Contact</a>
+			</div>
+		</div>
+	</div>
 	<div class="flex justify-center">
 		<div class="w-8/12 bg-white p-6 rounded-lg">
 			Profile
